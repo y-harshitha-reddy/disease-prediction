029a4e7719560dd17085504fec8bc55164c4d552
y-harshitha-reddy <yeddulaharshitha2005@gmail.com>
2025-03-09 13:45:26 +0530

    Update import streamlit as st.py

--- import streamlit as st.py
+++ import streamlit as st.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+pip install -r requirements.txt
+pip show statsmodels
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor, plot_tree
 from sklearn.metrics import accuracy_score, classification_report, mean_squared_error, r2_score
